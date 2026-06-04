877d2dd07b5cc5e5c3191bd69a2401b6179659d2
stellarwind23 <stellarwind@pm.me>
2026-06-04 13:21:49 -0500

    Switched to 32x32 & 48x48 textures, mainly just downscaled PBR textures for now

--- src/script/component/b_stats.gd
+++ src/script/component/b_stats.gd
@@ -3,3 +3,4 @@ class_name BStats extends Resource
 @export var break_time: int
 @export var e_resist: int
 @export var friction: float
+@export var bounciness: float
--- src/script/globals.gd
+++ src/script/globals.gd
@@ -1,5 +1,5 @@
 extends Node
 
-const texel_size: Vector2i = Vector2i(16, 16)
-const normal_mult: int = 3
+const texel_size: Vector2i = Vector2i(32, 32)
+const normal_mult: Vector2i = Vector2i(48, 48)
 const chunk_size: Vector3i = Vector3i(32, 32, 32)
