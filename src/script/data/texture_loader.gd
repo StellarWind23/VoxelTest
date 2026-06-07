d28d2609f35c9118e40a952bbbebb0de768723ff
stellarwind23 <stellarwind@pm.me>
2026-06-06 20:28:54 -0500

    Added a lot of textures.

--- src/script/data/texture_loader.gd
+++ src/script/data/texture_loader.gd
@@ -1,8 +1,46 @@
 @tool
 class_name TextureLoader extends Node
 
-var atlas: AtlasTexture
-var registry: Registry
+#Block
+var block_atlas: AtlasTexture
+var block_registry: Registry
+#Item
+var item_atlas: AtlasTexture
+var item_registry: Registry
+#Particle
+var particle_atlas: AtlasTexture
+var particle_registry: Registry
+#GUI
+var gui_atlas: AtlasTexture
+var gui_registry: Registry
+#Entity
+var entity_atlas: AtlasTexture
+var entity_registry: Registry
+
+static var _instance: TextureLoader
+
+func instance():
+	return self._instance
 
 func _ready() -> void:
-	registry = Registry.new()
+	#Block
+	block_registry = Registry.new()
+	block_atlas = AtlasTexture.new()
+	#Block
+	item_registry = Registry.new()
+	item_atlas = AtlasTexture.new()
+	#particle
+	particle_registry = Registry.new()
+	particle_atlas = AtlasTexture.new()
+	#gui
+	gui_registry = Registry.new()
+	gui_atlas = AtlasTexture.new()
+	#entity
+	entity_registry = Registry.new()
+	entity_atlas = AtlasTexture.new()
+	
+	
+	#Set instance
+	_instance = self
+	
+	#Load png files.
